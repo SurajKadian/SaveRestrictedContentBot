285d7cbb970906448d56fcd48e4d66684adb46e2
SurajKadian <138515654+SurajKadian@users.noreply.github.com>
2023-09-07 16:36:25 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID =25771027
+API_HASH ="2f5282a8905193510998342310d7853d"
+BOT_TOKEN ="6316167652:AAFCoXZEeQ14iE9EP9is8X1Kwg4LNmQk3ik"
+SESSION ="BQCKUMzkIC5bT3porcQGNB95m_SBi7_jtDseJDuym6kis0RGrPIzkzA7QIseEDpWyo0PRSFskbUcFCNZMjFU5UQT26H8thInETIfmUFY4PbVN-wCVd03ic_4aE78Ef3sO6d67rPPh7yyIg-S0aRXZ2CQNFPw8TnQfXdcKTqFcQ6lIm58i37N78AcC_UG_QCKQnkHZQKBsmA2hkgWcAcLAU9nHGBdry8n6-TWQM6VLsPzqzkctJ4ywn4znBP6s-useD90jri9nQAAw9TnJu6ZlmtfGlhbIZwOTgQzcvzrCh_nwmeQ4oPd4T1j9wrjhgWQlhWg9GXdNs8sYJAf4k1Kz27mAAAAAXkM8fcA"
+FORCESUB ="k00ntentsavebot"
+AUTH =6325858807
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
